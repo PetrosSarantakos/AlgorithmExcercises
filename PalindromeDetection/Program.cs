e711d780303233f0e0867b6c5093f61f1602dc7c
Petros Sarantakos <petros_sa@hotmail.com>
2023-09-10 17:30:58 +0300

    Added new projects, fixed minor errors

---
+++ PalindromeDetection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Palindrome
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Give me a string\n");
+        string n = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(n))
+        {
+            Console.WriteLine("String is null or empty");
+        }
+        else
+        {
+            string result = ReverseString(n);
+            if (result.ToLower() == n.ToLower())
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("No Palindrome");
+            }
+        }
+
+    }
+
+    private static string ReverseString(string? n)
+    {
+        char[] stringChars = n.ToCharArray();
+        string result = "";
+
+        for (int i = n.Length - 1; i >= 0; i--)
+        {
+            result += stringChars[i];
+        }
+
+        return result;
+    }
+
+}
---
+++ Recursion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Factorial
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Give me an int\n");
+        string n = Console.ReadLine();
+        var isInputValid = int.TryParse(n, out int num);
+        if (!isInputValid)
+            Console.WriteLine("Invalid input");
+        else
+        {
+            int result = CalculateFactorial(num);
+            Console.WriteLine(result);
+        }
+
+    }
+
+    public static int CalculateFactorial(int num)
+    {
+        int result = 1;
+        if (num == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            result = num * CalculateFactorial(num - 1);
+        }
+        return result;
+    }
+}
