9b0c24d8f75ba634c894ab20ebeb56f80c492e23
Petros Sarantakos <petros_sa@hotmail.com>
2023-09-10 17:00:43 +0300

    Add project files.

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class FibonacciSequence
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Give me an int\n");
+        string n = Console.ReadLine();
+        var isInputValid=int.TryParse(n, out int num);
+        if (isInputValid)
+            Console.WriteLine("Invalid input");
+        else
+        {
+            int result = CalculateFibonacciSeq(num);
+            Console.WriteLine(result);
+        }
+      
+    }
+
+    public static int CalculateFibonacciSeq(int num)
+    {
+        int fibonacciSum = 0;
+        int temp = 1;
+        for (int i = 0; i <= num; i++)
+        {
+            var old = fibonacciSum;
+            fibonacciSum += temp;
+            temp = old;
+            Console.WriteLine(fibonacciSum);
+        }
+        return fibonacciSum;
+    }
+}
---
+++ StringReverse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class StringReverse
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Give me a string\n");
+        string n = Console.ReadLine();
+        
+        if(string.IsNullOrEmpty(n))
+        {
+            Console.WriteLine("String is null or empty");
+        }
+        else
+        {
+            string result = ReverseString(n);
+            Console.WriteLine(result);
+        }
+
+    }
+
+    private static string ReverseString(string? n)
+    {
+        char[] stringChars = n.ToCharArray();
+        string result = "";
+
+        for(int i = n.Length - 1; i >= 0; i--)
+        {
+            result += stringChars[i];
+        }
+
+        return result;
+    }
+
+}
